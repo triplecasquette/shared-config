7688c6aad1d23f42a0e5b05b1d470109da452e14
Vincent Cottalorda <81014220+triplecasquette@users.noreply.github.com>
2025-07-18 13:58:38 +0200

    Update prettier.config.cjs

--- shared/prettier.config.cjs
+++ shared/prettier.config.cjs
@@ -1,8 +1,10 @@
-module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
   singleQuote: true,
   semi: true,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   printWidth: 140,
-  tabWidth: 2
-}
+  tabWidth: 2,
+};
+
+export default config;
